5bb7a80381edaee2a3bfccba655cfc38eea4d387
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2023-09-22 15:43:17 -0400

    scripts no longer validate vdi ids

--- lib/includes.sh
+++ lib/includes.sh
@@ -68,50 +68,6 @@ verifyFile() {
   return 0
 }
 
-
-# Verify VDI ID
-#
-# Ensures that the given VDI ID resembles a valid VDI ID in that it is a 32
-# character hex value.
-#
-# Signature:
-#   verifyVDIID <vdi-id>
-#
-# Parameters:
-#   vdi-id  - String value to test.
-#
-# Return:
-#   0 = The given value resembles a valid VDI dataset ID.
-#   1 = The given value does not resemble a valid VDI dataset ID.
-#
-# Example Usage:
-#   verifyVDIID $SOME_VALUE || exit $EXIT_CODE_UNEXPECTED_ERROR
-verifyVDIID() {
-  vdi_id="$1"
-
-  if [ -z "$vdi_id" ]; then
-    logMessage "required VDI ID parameter was blank or absent"
-    return 1
-  fi
-
-  if [ ! 32 -eq "${#vdi_id}" ]; then
-    logMessage "invalid VDI ID length"
-    return 1
-  fi
-
-  for (( i=0; i < 32; i++ )); do
-    c="${vdi_id:i:1}"
-
-    if ! isHex c; then
-      logMessage "invalid VDI ID"
-      return 1
-    fi
-  done
-
-  return 0
-}
-
-
 verifyEnv() {
   if [ -z "$2" ]; then
     logMessage "required environment variable $1 is blank or unset"
